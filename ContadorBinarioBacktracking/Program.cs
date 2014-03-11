0d6db5a44a63e4768d722e325873229c46174b5d
anderZubi <anderzubi@gmail.com>
2014-03-11 20:29:36 +0100

    ContadorBinario commit

---
+++ ContadorBinarioBacktracking/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ContadorBinarioBacktracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (!String.IsNullOrEmpty(input))
+            {
+                string result = "";
+                BinaryCounter(int.Parse(input), result);
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void BinaryCounter(int n, string result)
+        {
+            if (n == result.Length)
+                Console.WriteLine(result);
+            else
+            {
+                BinaryCounter(n, result + "0");
+                BinaryCounter(n, result + "1");
+            }
+        }
+    }
+}
---
+++ FactoresPrimos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FactoresPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (!String.IsNullOrEmpty(input))
+            {
+                int value;
+                List<int> result; ;
+
+                if (int.TryParse(input, out value) && value > 0)
+                {
+                    result = FactoresPrimos(int.Parse(input));
+                    Console.WriteLine(String.Join(",", result));
+                }
+                else
+                    Console.WriteLine("Por favor, inserta un número de tipo entero y mayor que 0.");
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static List<int> FactoresPrimos(int p)
+        {
+            List<int> result = new List<int>();
+            if (p == 1)
+                result.Add(p);
+            else
+            {           
+                while (p>1)
+                {
+                    int div = 2;
+
+                    while (p % div != 0)
+                        div++;
+
+                    p = p / div;
+                    result.Add(div);
+                }
+            }
+
+            return result;                
+        }
+    }
+}
