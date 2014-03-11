b2ed42f269f6dd4af5aff2d6cc1c11f8f43f75bc
anderzubi <anderzubi@gmail.com>
2014-03-11 18:19:59 +0100

    Divisores commit

---
+++ Divisores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Divisores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (!String.IsNullOrEmpty(input))
+            {
+                int value;
+                List<int> result; ;
+
+                if (int.TryParse(input, out value) && value > 0)
+                {
+                    result = Divisores(int.Parse(input));
+                    Console.WriteLine(String.Join(",", result.OrderBy(x => x)));
+                }
+                else
+                    Console.WriteLine("Por favor, inserta un número de tipo entero y mayor que 0.");
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static List<int> Divisores(int p)
+        {
+            List<int> result = new List<int>();
+            int max = p;
+
+            for (int i = 1; i < max; i++)
+            {
+                if (p % i == 0)
+                {
+                    int div = p / i;
+                    result.Add(i);
+                    result.Add(div);
+                    max = div;
+                }
+            }
+
+            return result;
+        }
+    }
+}
